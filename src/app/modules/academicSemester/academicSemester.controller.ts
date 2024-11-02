de93ae278a770d2afc1ccbfbd5427d787224db53
Sawom <asawom250@gmail.com>
2024-11-02 22:39:05 +0600

    academic route

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,22 @@
+import httpStatus from "http-status";
+import sendResponse from "../../utils/sendResponse";
+// import { UserServices } from "./user.service";
+import { RequestHandler } from "express";
+import catchAsync from "../../utils/catchAsync";
+
+// utils folder theke ashche. oikhane note kora ache
+const createAcademicSemester: RequestHandler = catchAsync(async (req, res) => {
+//   const { password, student: studentData } = req.body;
+//   const result = await UserServices.createStudentIntoDB(password, studentData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "student is created successfully",
+    data: result,
+  });
+});
+
+export const AcademicSemesterControllers = {
+  createAcademicSemester,
+};
