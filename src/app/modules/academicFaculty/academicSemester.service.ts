de9520a22cce097e2aadb5d06a132e7d413de930
Sawom <asawom250@gmail.com>
2024-11-05 14:40:25 +0600

    faculty service

---
+++ src/app/modules/academicFaculty/academicSemester.service.ts
@@ -0,0 +1,34 @@
+import { TAcademicFaculty } from "./academicFaculty.interface";
+import { AcademicFacultyModel } from "./academicFaculty.model";
+
+// create faculty
+const createAcademicFacultyIntoDB = async(payload: TAcademicFaculty ) =>{
+    const result = await AcademicFacultyModel.create(payload);
+    return result;
+}
+
+// getall faculty
+const getAllAcademicFacultiesFromDB = async() =>{
+    const result = await AcademicFacultyModel.find();
+  return result;
+}
+
+// single faculty load
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFacultyModel.findById(id);
+  return result;
+};
+
+// update faculty load
+const updateAcademicFacultyIntoDB = async( id: string, payload: Partial<TAcademicFaculty> ) =>{
+    const result = await AcademicFacultyModel.findOneAndUpdate({_id: id}, payload,{
+        new: true,
+    })
+    return result;
+}
+
+export const AcademicFacultyServices ={
+    getAllAcademicFacultiesFromDB,
+    getSingleAcademicFacultyFromDB,
+    updateAcademicFacultyIntoDB
+}
