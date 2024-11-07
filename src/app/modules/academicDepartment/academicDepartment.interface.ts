0338f723db508e27130de168c8aa16a681c92355
Sawom <asawom250@gmail.com>
2024-11-07 23:40:37 +0600

    department model

--- src/app/modules/academicDepartment/academicDepartment.interface.ts
+++ src/app/modules/academicDepartment/academicDepartment.interface.ts
@@ -3,6 +3,6 @@ import { Types } from "mongoose";
 export type TAcademicDepartment = {
     name: string;
     // academicDepartment k academicFaculty er sathe referencing korte hobe.
-    //  cz same facultyr under e onk department thakte pare
+    // cz same facultyr under e onk department thakte pare
     academicFaculty: Types.ObjectId;
 }
---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,58 @@
+import httpStatus from "http-status";
+import { Schema, model } from "mongoose";
+import AppError from "../../errors/AppError";
+import { TAcademicDepartment } from "./academicDepartment.interface";
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    // academicDepartment k academicFaculty er sathe referencing korte hobe.
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicFaculty",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// save
+academicDepartmentSchema.pre("save", async function (next) {
+  const isDepartmentExist = await AcademicDepartmentModel.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "This department is already exist!"
+    );
+  }
+
+  next();
+});
+
+// find one and update data
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartmentModel.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      "This department does not exist! "
+    );
+  }
+
+  next();
+});
+
+export const AcademicDepartmentModel = model<TAcademicDepartment>(
+  "AcademicDepartment",
+  academicDepartmentSchema
+);
