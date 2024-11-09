63c3e88082b081309edd7b6b8a66167f60ef4f96
Sawom <asawom250@gmail.com>
2024-11-09 13:37:00 +0600

    router set

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,13 +1,24 @@
-import  express  from "express";
+import express from "express";
+import validateRequest from "../../middlewares/validRequest";
 import { StudentController } from "./student.controller";
+import { updateStudentValidationSchema } from "./student.zod.validation";
 
 // route, controller, service. new route banale sathe sathe controller & service eo update kora lage
 const router = express.Router();
 
 // will call controller
 // router.post('/create-student', StudentController.createStudent )
-router.get('/', StudentController.getAllstudents);
-router.get('/:studentId', StudentController.getSinglestudent);
-router.delete('/:studentId', StudentController.deleteStudent);
+// get all student
+router.get("/", StudentController.getAllstudents);
+// get single student
+router.get("/:studentId", StudentController.getSinglestudent);
+// delete student
+router.delete("/:studentId", StudentController.deleteStudent);
+// update student
+router.patch(
+  "/:studentId",
+  validateRequest(updateStudentValidationSchema),
+  StudentController.updateStudent
+);
 
 export const StudentRoutes = router;
