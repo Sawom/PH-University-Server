8ab4f922ed696de7c9797db35a031296ddca8d83
Sawom <asawom250@gmail.com>
2024-11-13 02:32:20 +0600

    server error handler

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+import { Server } from "http";
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(` app listening on port ${config.port}`);
     });
   } catch (err) {
@@ -15,3 +18,21 @@ async function main() {
 }
 
 main();
+
+// *** globalHandleError shudhu express er error handle kore. ***
+// asynchronus code e error hole unhandledRejection error hoy.eta handle korte hoy
+process.on('unhandledRejection', (err) => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// synchronus code e error hole uncaughtException error hoy. eta handle korte hoy
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
