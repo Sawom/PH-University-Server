78305552da6cf92edd5309036f1fcbf5effa11d8
Sawom <asawom250@gmail.com>
2024-11-14 14:10:29 +0600

    controller

---
+++ src/app/modules/Admin/admin.route.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { AdminControllers } from './admin.controller';
+import validateRequest from '../../middlewares/validRequest';
+
+const router = express.Router();
+
+router.get('/', AdminControllers.getAllAdmins);
+
+router.get('/:id', AdminControllers.getSingleAdmin);
+
+router.patch(
+  '/:id',
+  validateRequest(updateAdminValidationSchema),
+  AdminControllers.updateAdmin,
+);
+
+router.delete('/:adminId', AdminControllers.deleteAdmin);
+
+export const AdminRoutes = router;
