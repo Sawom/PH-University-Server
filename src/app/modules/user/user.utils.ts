465161f7b4002ad0a2d33713d14d9952904e23c9
Sawom <asawom250@gmail.com>
2024-11-26 18:58:29 +0600

    admin faculty id generated

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -55,23 +55,70 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
 };
 
 // faculty id
-export const findLastFacultyId = async() =>{
+export const findLastFacultyId = async () => {
   const lastFaculty = await User.findOne(
     {
-      role: 'faculty',
+      role: "faculty",
     },
     {
       id: 1,
       _id: 0,
     }
-  ).sort({
-    createAt: -1,
-  }).lean();
+  )
+    .sort({
+      createAt: -1,
+    })
+    .lean();
 
   return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
-}
+};
 
 // faculty id generate
-export const generateFacultyId = async() =>{
+export const generateFacultyId = async () => {
+  let currentId = (0).toString();
+  const lastFacultyId = await findLastFacultyId();
 
+  if (lastFacultyId) {
+    currentId = lastFacultyId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `F-${incrementId}`;
+
+  return incrementId;
+};
+
+// admin id 
+export const findLastAdminId = async()=>{
+  const lastAdmin = await User.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  ).sort({
+    createAt: -1,
+  })
+  .lean()
+
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+} 
+
+// generate admin id
+export const generateAdminId =async() =>{
+  let currentId = (0).toString();
+  const lastAdminId = await findLastAdminId();
+
+  if(lastAdminId) {
+    currentId = lastAdminId.substring(2)
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `A-${incrementId}`;
+
+  return incrementId;
 }
