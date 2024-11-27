75e0a5f9882a6f27b22bfa888218f79aca7cb07c
Sawom <asawom250@gmail.com>
2024-11-27 21:19:15 +0600

    auth service

---
+++ src/app/modules/Auth/auth.service.ts
@@ -0,0 +1,59 @@
+import httpStatus from "http-status";
+import config from "../../config";
+import AppError from "../../errors/AppError";
+import { User } from "../user/user.model";
+import { TLoginUser } from "./auth.interface";
+import { createToken } from "./auth.utils";
+
+const loginUser = async (payload: TLoginUser) => {
+  // checking if the user is exist
+  const user = await User.isUserExistsByCustomId(payload.id);
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
+  }
+  // checking if the user is already deleted
+
+  const isDeleted = user?.isDeleted;
+
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !");
+  }
+
+  // checking if the user is blocked
+
+  const userStatus = user?.status;
+
+  if (userStatus === "blocked") {
+    throw new AppError(httpStatus.FORBIDDEN, "This user is blocked ! !");
+  }
+
+  //checking if the password is correct
+  if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
+    throw new AppError(httpStatus.FORBIDDEN, "Password do not matched");
+  }
+
+  //create token and sent to the  client
+  const JwtPayload = {
+    userId: user.id,
+    role: user.role,
+  };
+
+  const accessToken = createToken(
+    JwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string
+  );
+
+  const refreshToken = createToken(
+    JwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    needsPasswordChange: user?.needsPasswordChange,
+  };
+};
