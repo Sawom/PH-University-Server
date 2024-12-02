8088923eb8f9e32ab2e2aff0e1b23d94e348ff4d
Sawom <asawom250@gmail.com>
2024-12-02 19:52:00 +0600

    service done

--- src/app/modules/EnrolledCourse/enrolledCourse.utils.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.utils.ts
@@ -1,46 +1,47 @@
 export const calculateGradeAndPoints = (totalMarks: number) => {
   let result = {
-    grade: "NA",
+    grade: 'NA',
     gradePoints: 0,
   };
-  /** grade scale
+
+  /**
    * 0-19 F
    * 20-39 D
    * 40-59 C
    * 60-79 B
    * 80-100 A
    */
-
   if (totalMarks >= 0 && totalMarks <= 19) {
     result = {
-      grade: "F",
+      grade: 'F',
       gradePoints: 0.0,
     };
   } else if (totalMarks >= 20 && totalMarks <= 39) {
     result = {
-      grade: "D",
+      grade: 'D',
       gradePoints: 2.0,
     };
   } else if (totalMarks >= 40 && totalMarks <= 59) {
     result = {
-      grade: "C",
+      grade: 'C',
       gradePoints: 3.0,
     };
   } else if (totalMarks >= 60 && totalMarks <= 79) {
     result = {
-      grade: "B",
+      grade: 'B',
       gradePoints: 3.5,
     };
   } else if (totalMarks >= 80 && totalMarks <= 100) {
     result = {
-      grade: "A",
+      grade: 'A',
       gradePoints: 4.0,
     };
   } else {
     result = {
-      grade: "NA",
+      grade: 'NA',
       gradePoints: 0,
     };
   }
 
+  return result;
 };
