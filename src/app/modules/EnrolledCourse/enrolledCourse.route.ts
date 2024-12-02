6fe54760b79ee860cc1780a98af3309327408e8b
Sawom <asawom250@gmail.com>
2024-12-02 20:07:04 +0600

    route controller

---
+++ src/app/modules/EnrolledCourse/enrolledCourse.route.ts
@@ -0,0 +1,39 @@
+import auth from "../../middlewares/auth";
+import  express  from 'express';
+import { USER_ROLE } from "../user/user.constant";
+import validateRequest from "../../middlewares/validRequest";
+
+
+const router = express.Router();
+
+router.post(
+  '/create-enrolled-course',
+  auth(USER_ROLE.student),
+  validateRequest(
+    EnrolledCourseValidations.createEnrolledCourseValidationZodSchema,
+  ),
+  EnrolledCourseControllers.createEnrolledCourse,
+);
+
+router.get(
+  '/',
+  auth(USER_ROLE.faculty),
+  EnrolledCourseControllers.getAllEnrolledCourses,
+);
+
+router.get(
+  '/my-enrolled-courses',
+  auth(USER_ROLE.student),
+  EnrolledCourseControllers.getMyEnrolledCourses,
+);
+
+router.patch(
+  '/update-enrolled-course-marks',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  validateRequest(
+    EnrolledCourseValidations.updateEnrolledCourseMarksValidationZodSchema,
+  ),
+  EnrolledCourseControllers.updateEnrolledCourseMarks,
+);
+
+export const EnrolledCourseRoutes = router;
