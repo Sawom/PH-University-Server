fe8333ea646b2429f149e7a77f6b89fa20b94dbc
Sawom <asawom250@gmail.com>
2024-12-04 19:20:49 +0600

    route

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -1,25 +1,44 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const loginValidationSchema = z.object({
   body: z.object({
-    id: z.string({ required_error: 'Id is required.' }),
-    password: z.string({ required_error: 'Password is required' }),
+    id: z.string({ required_error: "Id is required." }),
+    password: z.string({ required_error: "Password is required" }),
   }),
 });
 
 const changePasswordValidationSchema = z.object({
   body: z.object({
     oldPassword: z.string({
-      required_error: 'Old password is required',
+      required_error: "Old password is required",
     }),
-    newPassword: z.string({ required_error: 'Password is required' }),
+    newPassword: z.string({ required_error: "Password is required" }),
   }),
 });
 
 const refreshTokenValidationSchema = z.object({
   cookies: z.object({
     refreshToken: z.string({
-      required_error: 'Refresh token is required!',
+      required_error: "Refresh token is required!",
+    }),
+  }),
+});
+
+const forgetPasswordValidationSchema = z.object({
+  body: z.object({
+    id: z.string({
+      required_error: "User id is required!",
+    }),
+  }),
+});
+
+const resetPasswordValidationSchema = z.object({
+  body: z.object({
+    id: z.string({
+      required_error: "User id is required!",
+    }),
+    newPassword: z.string({
+      required_error: "User password is required!",
     }),
   }),
 });
@@ -28,4 +47,6 @@ export const AuthValidation = {
   loginValidationSchema,
   changePasswordValidationSchema,
   refreshTokenValidationSchema,
+  forgetPasswordValidationSchema,
+  resetPasswordValidationSchema,
 };
