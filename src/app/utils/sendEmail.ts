9421593681d2205f9b6c31911dc02fb96c9b1ee1
Sawom <asawom250@gmail.com>
2024-12-04 21:55:01 +0600

    updated

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -3,13 +3,14 @@ import config from "../config";
 
 export const sendEmail = async (to: string, html: string) => {
   const transporter = nodemailer.createTransport({
+    // host , port gmail er jnno fixed
     host: "smtp.gmail.com.",
     port: 587,
     secure: config.NODE_ENV === "production",
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
       user: "asawom250@gmail.com",
-      pass: "xfqj dshz wdui ymtb",
+      pass: "gjxo kafj xzxk nyxw",
     },
   });
 
