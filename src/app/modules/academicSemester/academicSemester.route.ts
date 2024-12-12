cc7c07462157dad9be1a27dbb94ca3c437e94ef1
Sawom <asawom250@gmail.com>
2024-12-12 22:11:49 +0600

    semester route

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,29 +1,54 @@
-import express from 'express';
-import { AcademicSemesterControllers } from './academicSemester.controller';
-import validateRequest from '../../middlewares/validRequest';
-import { AcademicSemesterValidations } from './academicSemester.validation';
+import express from "express";
+import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validRequest";
+import { USER_ROLE } from "../user/user.constant";
+import { AcademicSemesterControllers } from "./academicSemester.controller";
+import { AcademicSemesterValidations } from "./academicSemester.validation";
 
 const router = express.Router();
 
 // create semester
-router.post('/create-academic-semester', validateRequest(AcademicSemesterValidations.createAcademicSemesterValidationSchema), AcademicSemesterControllers.createAcademicSemester, )
+router.post(
+  "/create-academic-semester",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validateRequest(
+    AcademicSemesterValidations.createAcademicSemesterValidationSchema
+  ),
+  AcademicSemesterControllers.createAcademicSemester
+);
 
 // get single semester
 router.get(
-  '/:semesterId',
-  AcademicSemesterControllers.getSingleAcademicSemester,
+  "/:semesterId",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student
+  ),
+  AcademicSemesterControllers.getSingleAcademicSemester
 );
 
 // update semester
 router.patch(
-  '/:semesterId',
+  "/:semesterId",
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(
-    AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
+    AcademicSemesterValidations.updateAcademicSemesterValidationSchema
   ),
-  AcademicSemesterControllers.updateAcademicSemester,
+  AcademicSemesterControllers.updateAcademicSemester
 );
 
 // get all data academ
-router.get('/', AcademicSemesterControllers.getAllAcademicSemesters);
+router.get(
+  "/",
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student
+  ),
+  AcademicSemesterControllers.getAllAcademicSemesters
+);
 
 export const AcademicSemesterRoutes = router;
