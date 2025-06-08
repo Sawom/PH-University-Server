860acb95aa63ef27421a99751a01bb0e7e25162f
Sawom <asawom250@gmail.com>
2025-06-08 23:14:00 +0600

    updated

--- src/app/modules/Course/course.interface.ts
+++ src/app/modules/Course/course.interface.ts
@@ -11,7 +11,7 @@ export type TCourse = {
   code: number;
   credits: number;
   isDeleted?: boolean;
-  preRequisiteCourses: [TPreRequisiteCourses];
+  preRequisiteCourses?: [TPreRequisiteCourses];
 };
 
 // same course multiple faculty nite paren. tai faculty ary nea
